7452b37d7d6c0e641349f35b8788cf251ebfe059
Mateusz Kolodziejczyk <vyax9px2@duck.com>
2024-04-22 16:30:51 +0200

    feat: add implementation of the DNS lookup for local server

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"dns-client-go/dns"
 	packetbuffer "dns-client-go/packetbuffer"
 	querytype "dns-client-go/query-type"
+	resultcode "dns-client-go/result-code"
 	"fmt"
 	"net"
 )
@@ -40,57 +41,72 @@ func lookup(qname string, qtype querytype.QueryType) (*dns.DnsPacket, error) {
 	return rawPacket.FromBuffer(&bpb), nil
 }
 
-func main() {
-	name := "yahoo.com"
-	qtype := querytype.NS
+func handleQuery(conn *net.UDPConn) error {
+	requestBuffer := packetbuffer.NewPacketBuffer()
+	_, src, err := conn.ReadFromUDP(requestBuffer.Buffer)
 
-	server := "8.8.8.8:53"
-
-	conn, err := net.Dial("udp", server)
 	if err != nil {
-		fmt.Println(err)
-		return
+		return fmt.Errorf("failed to read from UDP Socket: %w", err)
+	}
+
+	reqPacket := dns.NewPacket()
+	request := reqPacket.FromBuffer(&requestBuffer)
+
+	response := dns.NewPacket()
+	response.Header.ID = request.Header.ID
+	response.Header.RecursionDesired = true
+	response.Header.RecursionAvailable = true
+	response.Header.Response = true
+
+	if len(request.Question) > 0 {
+		question := request.Question[0]
+
+		result, err := lookup(question.Name, question.Qtype)
+		if err != nil {
+			response.Header.Rescode = resultcode.SERVFAIL
+		} else {
+			response.Question = append(response.Question, question)
+			response.Header.Rescode = result.Header.Rescode
+			response.Answers = result.Answers
+			response.Authorities = result.Authorities
+			response.Resources = result.Resources
+		}
+	} else {
+		response.Header.Rescode = resultcode.FORMERR
+	}
+
+	resBuffer := packetbuffer.NewPacketBuffer()
+	response.Write(&resBuffer)
+
+	len := resBuffer.Pos()
+	data, err := resBuffer.GetRange(0, len)
+	if err != nil {
+		return fmt.Errorf("failed to serialize response packet: %w", err)
+	}
+
+	_, err = conn.WriteToUDP(data, src)
+	if err != nil {
+		return fmt.Errorf("failed to send response: %w", err)
+	}
+
+	return nil
+}
+
+func main() {
+	addr := net.UDPAddr{
+		Port: 2053,
+		IP:   net.ParseIP("0.0.0.0"),
+	}
+	conn, err := net.ListenUDP("udp", &addr)
+	if err != nil {
+		panic(err)
 	}
 	defer conn.Close()
 
-	rawPacket := &dns.DnsPacket{}
-
-	rawPacket.Header.ID = 1
-	rawPacket.Header.RecursionDesired = true
-	question := dns.NewQuestion(name, qtype)
-	rawPacket.Question = append(rawPacket.Question, *question)
-	rawPacket.Header.Questions = 1
-
-	requestBuffer := packetbuffer.NewPacketBuffer()
-	rawPacket.Write(&requestBuffer)
-
-	conn.Write(requestBuffer.Buffer)
-
-	bpb := packetbuffer.NewPacketBuffer()
-
-	_, err = conn.Read(bpb.Buffer)
-	if err != nil {
-		fmt.Println(err)
-		return
+	fmt.Println("DNS server listening on port 2053")
+	for {
+		if err := handleQuery(conn); err != nil {
+			fmt.Println("Error handling query:", err)
+		}
 	}
-
-	packet := rawPacket.FromBuffer(&bpb)
-
-	fmt.Printf("%+v\n", packet.Header)
-	for _, rec := range packet.Question {
-		fmt.Printf("%+v\n", rec)
-	}
-
-	for _, rec := range packet.Answers {
-		fmt.Printf("%+v\n", rec)
-	}
-
-	for _, rec := range packet.Authorities {
-		fmt.Printf("%+v\n", rec)
-	}
-
-	for _, rec := range packet.Resources {
-		fmt.Printf("%+v\n", rec)
-	}
-
 }
